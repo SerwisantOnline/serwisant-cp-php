3375c1d9b2855c1df4022af514a5f54fdf992ab8
Arkadiusz Kuryłowicz <arkadiusz@kurylowicz.info>
2021-05-30 18:56:44 +0200

    Refactoring, new assets

--- public/index.php
+++ public/index.php
@@ -1,18 +1,31 @@
 <?php
-
-$env = (stripos($_SERVER['SERVER_SOFTWARE'], 'development server') !== false ? 'development' : 'production');
+/**
+ * Ustaw zmienne środowiskowe (zalecane) lub podaj tutaj wprost dane autoryzacyjne aplikacji OAuth.
+ *
+ * Jeśli otrzymujesz białą stronę zamiast panelu klienckiego ustaw zmienną środowiskową PHP_ENV=development - dzięki
+ * temu zobaczysz jakie błędy wyrzuca aplikacja. Po uruchomieniu usuń tę zmienną z uwago na bezpieczeństwo aplikacji.
+ */
 
 $oauth_key = getenv('OAUTH_KEY');
 $oauth_secret = getenv('OAUTH_SECRET');
+$env = getenv('PHP_ENV');
+
+/**
+ * Jeśli chcesz korzystać, ze współdzielonego katalogu na pliki tymczasowe i sesje usuń poniższą linijkę, przy czym
+ * zwróć uwagę na to, że ich zawartość może być dostępna dla innych jeśli używasz współdzielonego hostingu.
+ */
 
 putenv('TMPDIR=' . realpath(__DIR__ . '/../tmp'));
 
-error_reporting(E_ALL);
-ini_set('display_errors', ($env === 'development'));
+/**
+ * Nie ma potrzeby edytowania czegokolwiek poniżej.
+ */
 
-ini_set('session.gc_maxlifetime', 3600 * 2);
-session_set_cookie_params(3600 * 2);
-session_start();
+if (!trim($env)) {
+  $env = 'production';
+}
+ini_set('display_errors', ($env == 'development'));
+error_reporting(E_ALL);
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
@@ -24,15 +37,13 @@ $application->setPublicAccessToken(new SerwisantApi\AccessTokenOauth(
   $oauth_key,
   $oauth_secret,
   'public',
-  new SerwisantApi\AccessTokenContainerEncryptedFile(sha1($oauth_secret)),
-  getenv('OAUTH_URL')
+  new SerwisantApi\AccessTokenContainerEncryptedFile(sha1($oauth_secret))
 ));
 $application->setCustomerAccessToken(new SerwisantApi\AccessTokenOauthUserCredentials(
   $oauth_key,
   $oauth_secret,
   'customer',
-  new SerwisantApi\AccessTokenContainerSession(),
-  getenv('OAUTH_URL')
+  new SerwisantApi\AccessTokenContainerSession()
 ));
-$application->setRouter(new SerwisantCp\RouterSelfHosted());
+$application->setRouter(new SerwisantCp\ApplicationRouter());
 $application->run();
