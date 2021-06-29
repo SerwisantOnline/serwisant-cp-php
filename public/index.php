635bd83279d8be8819d9a436393e022365c9adb3
Arkadiusz Kuryłowicz <arkadiusz@kurylowicz.info>
2021-06-30 00:03:15 +0200

    FA + refactor

--- public/index.php
+++ public/index.php
@@ -12,7 +12,7 @@ $env = getenv('PHP_ENV');
 
 /**
  * Jeśli chcesz korzystać, ze współdzielonego katalogu na pliki tymczasowe i sesje usuń poniższą linijkę, przy czym
- * zwróć uwagę na to, że ich zawartość może być dostępna dla innych jeśli używasz współdzielonego hostingu.
+ * zwróć uwagę na to, że ich zawartość może być dostępna dla innych.
  */
 
 putenv('TMPDIR=' . realpath(__DIR__ . '/../tmp'));
@@ -27,23 +27,28 @@ if (!trim($env)) {
 ini_set('display_errors', ($env == 'development'));
 error_reporting(E_ALL);
 
-require_once __DIR__ . '/../vendor/autoload.php';
+/** @noinspection PhpIncludeInspection */
+require_once realpath(__DIR__ . '/../vendor/autoload.php');
 
 use Serwisant\SerwisantCp;
 use Serwisant\SerwisantApi;
 
 $application = new SerwisantCp\Application($env);
+
 $application->setPublicAccessToken(new SerwisantApi\AccessTokenOauth(
   $oauth_key,
   $oauth_secret,
   'public',
   new SerwisantApi\AccessTokenContainerEncryptedFile(sha1($oauth_secret))
 ));
+
 $application->setCustomerAccessToken(new SerwisantApi\AccessTokenOauthUserCredentials(
   $oauth_key,
   $oauth_secret,
   'customer',
   new SerwisantApi\AccessTokenContainerSession()
 ));
+
 $application->setRouter(new SerwisantCp\ApplicationRouter());
+
 $application->run();
